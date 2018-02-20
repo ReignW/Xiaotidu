b6935770f66426db17d441db693926986d8abca4
QQ876684433 <876684433@qq.com>
2018-02-20 16:41:41 +0800

    完成邮箱验证码部分（有必要吐槽一下qq邮箱的ssl呵呵）

---
+++ app/src/main/java/com/njuse/utils/MailUtil.java
@@ -0,0 +1,66 @@
+package com.njuse.utils;
+
+import java.util.Date;
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ * Created by Administrator on 2018/2/20.
+ */
+
+public class MailUtil {
+//
+//    public static Properties createConfiguration() {
+//        return new Properties() {
+//            {
+//                put("mail.smtp.auth", "true");
+//                put("mail.smtp.host", "smtp.qq.com");
+//                put("mail.smtp.port", "587");
+//                put("mail.smtp.starttls.enable", "true");
+//            }
+//        };
+//    }
+//
+//    public static class SmtpAuthenticator extends Authenticator {
+//
+//        private String username = "876684433@qq.com";
+//        private String password = "zuhietzjajcpbbje";
+//
+//        @Override
+//        public PasswordAuthentication getPasswordAuthentication() {
+//            return new PasswordAuthentication(username, password);
+//        }
+//    }
+
+    /**
+     * 创建一封只包含文本的简单邮件
+     *
+     * @param session 和服务器交互的会话
+     * @param sendMail 发件人邮箱
+     * @param receiveMail 收件人邮箱
+     * @return
+     * @throws Exception
+     */
+    public static MimeMessage createMimeMessage(Session session, String sendMail, String receiveMail,String identifyingCode) throws Exception {
+        // 1. 创建一封邮件
+        MimeMessage message = new MimeMessage(session);
+        // 2. From: 发件人
+        message.setFrom(new InternetAddress(sendMail, "小题督", "UTF-8"));
+        // 3. To: 收件人（可以增加多个收件人、抄送、密送）
+        message.setRecipient(MimeMessage.RecipientType.TO, new InternetAddress(receiveMail, "亲爱的用户", "UTF-8"));
+        // 4. Subject: 邮件主题
+        message.setSubject("小题督用户注册验证码", "UTF-8");
+        // 5. Content: 邮件正文（可以使用html标签）
+        message.setContent(identifyingCode, "text/html;charset=UTF-8");
+        // 6. 设置发件时间
+        message.setSentDate(new Date());
+        // 7. 保存设置
+        message.saveChanges();
+        return message;
+    }
+}
--- app/src/main/java/com/njuse/xiaotidu/RegisterActivity.java
+++ app/src/main/java/com/njuse/xiaotidu/RegisterActivity.java
@@ -13,17 +13,29 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.njuse.utils.MailUtil;
+
+import java.util.Properties;
 import java.util.Random;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 /**
  * Created by Administrator on 2018/2/5.
  */
 
-public class RegisterActivity extends AppCompatActivity{
+public class RegisterActivity extends AppCompatActivity {
     private LinearLayout backToLogin;
     private TextView userAgreement;
     private EditText emailAddress;
     private Button identifyingCode;
+    boolean isSendSuccess;
+    String code;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -37,7 +49,7 @@ public class RegisterActivity extends AppCompatActivity{
         backToLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
                 finish();
             }
         });
@@ -48,7 +60,7 @@ public class RegisterActivity extends AppCompatActivity{
             public void onClick(View v) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this)
                         .setTitle("发送验证码")
-                        .setMessage("请确认你的邮箱地址：\n"+emailAddress.getText().toString()+"\n如果填写无误，稍后你将收到一封验证码邮件");
+                        .setMessage("请确认你的邮箱地址：\n" + emailAddress.getText().toString() + "\n如果填写无误，稍后你将收到一封验证码邮件");
 
                 setNegativeButton(builder);
                 setPositiveButton(builder);
@@ -70,15 +82,26 @@ public class RegisterActivity extends AppCompatActivity{
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         int rawCode = new Random().nextInt(999999);     //生成验证码
-                        String code = "111111";     //应改为String code = rawCode+"";
-                        boolean isSendSuccess = sendIdentifyingCode(code);
-                        if (isSendSuccess){
-                            startActivity(new Intent(RegisterActivity.this,IdentifyingCodeActivity.class)
-                                    .putExtra("email",emailAddress.getText().toString())
-                                    .putExtra("identifyingCode",code));
-                        }else {
-                            Toast.makeText(RegisterActivity.this,"验证码发送失败，请重试！",Toast.LENGTH_LONG).show();
-                        }
+                        code = rawCode + "";
+
+                        new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+
+                                    isSendSuccess = sendIdentifyingCode(emailAddress.getText().toString(), code);
+                                    if (isSendSuccess) {
+                                        startActivity(new Intent(RegisterActivity.this, IdentifyingCodeActivity.class)
+                                                .putExtra("email", emailAddress.getText().toString())
+                                                .putExtra("identifyingCode", code));
+                                    } else {
+                                        Toast.makeText(RegisterActivity.this, "验证码发送失败，请重试！", Toast.LENGTH_LONG).show();
+                                    }
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }).start();
 
                     }
                 });
@@ -87,10 +110,43 @@ public class RegisterActivity extends AppCompatActivity{
     }
 
     //发送验证码到用户邮箱
-    private boolean sendIdentifyingCode(String code) {
-        //To do something
+    public static boolean sendIdentifyingCode(final String receiver, final String identifyingCode) {
+        // 1. 创建参数配置, 用于连接邮件服务器的参数配置
+        Properties props = new Properties();          // 参数配置
+        props.setProperty("mail.transport.protocol", "smtp");  // 使用的协议（JavaMail规范要求）
+        props.setProperty("mail.smtp.host", "smtp.163.com");   // 发件人的邮箱的 SMTP 服务器地址
+        props.setProperty("mail.smtp.auth", "true");      // 需要请求认证
+        props.setProperty("mail.smtp.port", "25");
+        //如果遇到ssl类错误，请打开一下代码
+//        final String smtpPort = "587";
+//        props.setProperty("mail.smtp.port", smtpPort);
+//        props.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+//        props.setProperty("mail.smtp.socketFactory.fallback", "false");
+//        props.setProperty("mail.smtp.socketFactory.port", smtpPort);
+        // 2. 根据配置创建会话对象, 用于和邮件服务器交互
+        Session session = Session.getDefaultInstance(props);
+        // 设置为debug模式, 可以查看详细的发送 log
+        session.setDebug(true);
+        // 3. 创建一封邮件
+        MimeMessage message = null;//我这里是以163邮箱为发信邮箱测试通过
+        try {
+            message = MailUtil.createMimeMessage(session, "13420146901@163.com", receiver, "验证码："+identifyingCode+"\n(注意！请不要泄露给他人！)\n\n如果不是你本人操作，请忽略该邮件！请勿回复！");
+            // 4. 根据 Session 获取邮件传输对象
+            Transport transport = session.getTransport();
+            transport.connect("13420146901@163.com", "njuse2017");
+            // 6. 发送邮件, 发到所有的收件地址, message.getAllRecipients() 获取到的是在创建邮件对象时添加的所有收件人, 抄送人, 密送人
+            transport.sendMessage(message, message.getAllRecipients());
+            // 7. 关闭连接
+            transport.close();
+
+        } catch (Exception e) {
+            return false;
+//            e.printStackTrace();
+        }
+
         return true;
     }
+
 
     private void initWidget() {
         backToLogin = findViewById(R.id.back_to_login);
