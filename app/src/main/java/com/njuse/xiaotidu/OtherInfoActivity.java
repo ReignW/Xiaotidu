6186f5f73ea7eefb99fcf3770f115e1e7682c444
QQ876684433 <876684433@qq.com>
2018-02-21 11:32:22 +0800

    完成设置手机号和密码的正则匹配功能（为方便测试，验证码并没有真正发送，默认为111111）

--- app/src/main/java/com/njuse/xiaotidu/OtherInfoActivity.java
+++ app/src/main/java/com/njuse/xiaotidu/OtherInfoActivity.java
@@ -4,19 +4,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
 import android.text.InputType;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 /**
  * Created by Administrator on 2018/2/5.
  */
 
-public class OtherInfoActivity extends AppCompatActivity{
+public class OtherInfoActivity extends AppCompatActivity {
     private LinearLayout backToEmailInput;
     private EditText phoneNum;
     private EditText password;
@@ -25,6 +28,9 @@ public class OtherInfoActivity extends AppCompatActivity{
     private CheckBox showPassword;
 
     String emailAddress;    //邮箱地址
+
+    boolean isPhoneNumberMatch;
+    boolean isPasswordMatch;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -56,21 +62,93 @@ public class OtherInfoActivity extends AppCompatActivity{
         backToEmailInput.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(OtherInfoActivity.this,RegisterActivity.class));
+                startActivity(new Intent(OtherInfoActivity.this, RegisterActivity.class));
                 finish();
+            }
+        });
+
+        //手机号文本变化监听器
+        phoneNum.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String phoneNumPattern = "(13|14|15|18)[0-9]{9}";     //手机号正则表达式
+                if (phoneNum.getText().toString().matches(phoneNumPattern)) {
+                    isPhoneNumberMatch = true;
+                } else {
+                    isPhoneNumberMatch = false;
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        //密码输入框文本变化监听器
+        TextWatcher passwordChangedListener = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String passwordPattern = "[A-Za-z0-9_]{8,16}";      //密码匹配的正则表达式
+                if (password.getText().toString().equals("") || password.getText().toString().equals("")){
+                    return;
+                }
+                if (password.getText().toString().matches(passwordPattern) && passwordConfirm.getText().toString().equals(password.getText().toString())){
+                    isPasswordMatch = true;
+                }else {
+                    isPasswordMatch = false;
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        };
+        password.addTextChangedListener(passwordChangedListener);
+        passwordConfirm.addTextChangedListener(passwordChangedListener);
+
+        //注册按钮点击事件
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isPhoneNumberMatch){
+                    Toast.makeText(OtherInfoActivity.this,"手机号码的格式不正确，请修改！",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (!isPasswordMatch){
+                    Toast.makeText(OtherInfoActivity.this,"密码格式不正确或者两次密码输入不匹配，请修改！",Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                //如果信息正确，则提交后台注册用户信息
+
             }
         });
     }
 
     //初始化控件
     private void initWidget() {
-        backToEmailInput = findViewById(com.njuse.xiaotidu.R.id.back_to_email_input);
-        phoneNum = findViewById(com.njuse.xiaotidu.R.id.phone_number_edittext);
-        password = findViewById(com.njuse.xiaotidu.R.id.password_edittext);
-        passwordConfirm = findViewById(com.njuse.xiaotidu.R.id.password_confirm_edittext);
-        register = findViewById(com.njuse.xiaotidu.R.id.register_button);
-        showPassword = findViewById(com.njuse.xiaotidu.R.id.show_password_checkbox);
+        backToEmailInput = findViewById(R.id.back_to_email_input);
+        phoneNum = findViewById(R.id.phone_number_edittext);
+        password = findViewById(R.id.password_edittext);
+        passwordConfirm = findViewById(R.id.password_confirm_edittext);
+        register = findViewById(R.id.register_button);
+        showPassword = findViewById(R.id.show_password_checkbox);
 
         emailAddress = getIntent().getStringExtra("email");     //获取邮箱地址
+
+        isPasswordMatch = false;
+        isPhoneNumberMatch = false;
     }
 }
